e331a05b27ffdde3447e2889db4b53b180c25c74
Wesley A Siebenthaler <siebenwa@rose-hulman.edu>
2021-07-16 17:35:17 -0700

    more

--- src/main/java/sudoku/Launcher.java
+++ src/main/java/sudoku/Launcher.java
@@ -8,13 +8,21 @@ import main.java.sudoku.variants.modulus.ModDrawer;
 public class Launcher {
 
 	public static void main(String[] args) {
-		Puzzle puzzle = new Puzzle("resources/variants/test2.txt");
-		VariantDrawer modDrawer = new ModDrawer();
+		Puzzle puzzle = new Puzzle("resources/puzzles/formom");
+		//VariantDrawer modDrawer = new ModDrawer();
 		Drawer drawer = new Drawer();
-		drawer.addVariant(modDrawer);
+		//drawer.addVariant(modDrawer);
 		Frame frame = new Frame(drawer);
 		Listener listener = new Listener(puzzle, frame);
 		listener.refresh();
+		while(true) {
+			listener.refresh();
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+				
+			}
+		}
 	}
 
 }
